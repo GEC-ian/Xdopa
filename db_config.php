a5ad230a62c080600d22515b6dfb1a9c6af8e901
Gautam Kumar <69181323+PondyX@users.noreply.github.com>
2021-10-12 13:44:59 +0530

    Delete db_config.php

--- db_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host = 'localhost';
-$username = 'root';
-$password = '';
-$database = 'loginmanager';
-$dbconfig = mysqli_connect($host,$username,$password,$database) or die("An Error occured when connecting to the database");
-?>
