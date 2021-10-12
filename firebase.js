a87c45e52facf3413e8ff55aff66ac9010fd4bf8
Gautam Kumar <69181323+PondyX@users.noreply.github.com>
2021-10-12 14:12:36 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,15 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyCDL3U26Kvu16VxUcpsRtpL07dEIDwIgpQ",
-    authDomain: "fir-project-a7458.firebaseapp.com",
-    projectId: "fir-project-a7458",
-    storageBucket: "fir-project-a7458.appspot.com",
-    messagingSenderId: "575906290952",
-    appId: "1:575906290952:web:0f2f7421abffc0db1420bd",
-    measurementId: "G-6EF9ND3KRX"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+const firebaseConfig = {
+    apiKey: "AIzaSyD3q9yVRcPVAAQ985fc1BwKHQ_dxjuEFqM",
+    authDomain: "sqldatabase-5ab3f.firebaseapp.com",
+    databaseURL: "https://sqldatabase-5ab3f-default-rtdb.firebaseio.com",
+    projectId: "sqldatabase-5ab3f",
+    storageBucket: "sqldatabase-5ab3f.appspot.com",
+    messagingSenderId: "810030816524",
+    appId: "1:810030816524:web:679f9d8d078ab19752fd29",
+    measurementId: "G-C4GFX75GT7"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
